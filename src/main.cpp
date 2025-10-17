10d39f2092d11ea564dc03c6583e22a6ee5dffa3
FlauschBert <github@shaula.de>
2025-10-17 18:33:53 +0200

    add some constness, set random random seed

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,8 @@ void clearScreen() {
     }
 }
 
-void drawText(uint8_t posX, uint8_t posY, char const* text, uint8_t const size, uint8_t const color) {
+void drawText(uint8_t const posX, uint8_t const posY, char const* text,
+              uint8_t const size, uint8_t const color) {
     volatile uint8_t* screenPtr = (volatile uint8_t*)SCREEN_MEMORY;
     volatile uint8_t* colorPtr = (volatile uint8_t*)COLOR_MEMORY;
     for (uint8_t i = 0; i < size; ++i) {
@@ -35,43 +36,46 @@ void drawText(uint8_t posX, uint8_t posY, char const* text, uint8_t const size, 
     }
 }
 
-void drawToScreen(const uint8_t* src, uint8_t const srcWidth, uint8_t const srcHeight,
-                  uint8_t const srcOffsetX, uint8_t const srcOffsetY, uint8_t const screenX,
-                  uint8_t const screenY, uint8_t const rectWidth, uint8_t const rectHeight,
-                  uint8_t const rectColor) {
+void drawToScreen(const uint8_t* const src, uint8_t const srcWidth,
+                  uint8_t const srcHeight, uint8_t const srcOffsetX,
+                  uint8_t const srcOffsetY, uint8_t const screenX,
+                  uint8_t const screenY, uint8_t const rectWidth,
+                  uint8_t const rectHeight, uint8_t const rectColor) {
     volatile uint8_t* screenPtr = (volatile uint8_t*)SCREEN_MEMORY;
     volatile uint8_t* colorPtr = (volatile uint8_t*)COLOR_MEMORY;
 
     uint8_t screenXClamp = clamp(screenX, 0, SCREEN_WIDTH - 1);
     uint8_t screenYClamp = clamp(screenY, 0, SCREEN_HEIGHT - 1);
-    uint8_t screenXEndClamp = clamp(screenX + rectWidth - 1, 0, SCREEN_WIDTH - 1);
-    uint8_t screenYEndClamp = clamp(screenY + rectHeight - 1, 0, SCREEN_HEIGHT - 1);
-    
+    uint8_t screenXEndClamp =
+        clamp(screenX + rectWidth - 1, 0, SCREEN_WIDTH - 1);
+    uint8_t screenYEndClamp =
+        clamp(screenY + rectHeight - 1, 0, SCREEN_HEIGHT - 1);
+
     if (screenXClamp > screenXEndClamp || screenYClamp > screenYEndClamp) {
         return;
     }
 
-    uint8_t screenWidthClamped = screenXEndClamp - screenXClamp + 1;
-    uint8_t screenHeightClamped = screenYEndClamp - screenYClamp + 1;
+    uint8_t const screenWidthClamped = screenXEndClamp - screenXClamp + 1;
+    uint8_t const screenHeightClamped = screenYEndClamp - screenYClamp + 1;
 
-    uint8_t srcOffsetXAdjusted = srcOffsetX + (screenXClamp - screenX);
-    uint8_t srcOffsetYAdjusted = srcOffsetY + (screenYClamp - screenY);
+    uint8_t const srcOffsetXAdjusted = srcOffsetX + (screenXClamp - screenX);
+    uint8_t const srcOffsetYAdjusted = srcOffsetY + (screenYClamp - screenY);
 
     for (uint8_t y = 0; y < screenHeightClamped; ++y) {
         for (uint8_t x = 0; x < screenWidthClamped; ++x) {
-            
             // we assume that src is large enough to hold the requested data
-            uint8_t srcX = x + srcOffsetXAdjusted;
-            uint8_t srcY = y + srcOffsetYAdjusted;
-            uint16_t srcPos = srcY * (uint16_t)srcWidth + srcX;
+            uint8_t const srcX = x + srcOffsetXAdjusted;
+            uint8_t const srcY = y + srcOffsetYAdjusted;
+            uint16_t const srcPos = srcY * (uint16_t)srcWidth + srcX;
 
             if (srcPos >= srcWidth * srcHeight || srcPos < 0) {
                 continue;
             }
 
-            uint8_t screenXPos = x + screenXClamp;
-            uint8_t screenYPos = y + screenYClamp;
-            uint16_t screenPos = screenYPos * (uint16_t)SCREEN_WIDTH + screenXPos;
+            uint8_t const screenXPos = x + screenXClamp;
+            uint8_t const screenYPos = y + screenYClamp;
+            uint16_t const screenPos =
+                screenYPos * (uint16_t)SCREEN_WIDTH + screenXPos;
 
             screenPtr[screenPos] = src[srcPos];
             colorPtr[screenPos] = rectColor;
@@ -97,25 +101,26 @@ int main() {
     clearScreen();
 
     // Only uppercase letters are correctly shown in PETSCII
-    drawText(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4, "GENERATING TEST DATA ...", 24, 1);
+    drawText(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4, "GENERATING TEST DATA ...",
+             24, 1);
 
     fillCheckBoard(&gameArray[0][0], ' ', '#', ARRAY_WIDTH, ARRAY_HEIGHT);
 
     clearScreen();
 
-    // srand(12345);
+    // Use raster line counter as seed
+    srand(*(volatile uint8_t*)0xD012);
 
 #if 1
-    for (uint8_t i = 0; i < 100; ++i) {
-        uint8_t rectWidth = 5 + (rand() % 16);
-        uint8_t rectHeight = 5 + (rand() % 11);
-        uint8_t screenX = rand() % (SCREEN_WIDTH - rectWidth);
-        uint8_t screenY = rand() % (SCREEN_HEIGHT - rectHeight);
-        uint8_t color = 1 + (rand() % 15);
+    for (uint8_t i = 0; i < 250; ++i) {
+        uint8_t const rectWidth = 2 + (rand() % 16);
+        uint8_t const rectHeight = 2 + (rand() % 9);
+        uint8_t const screenX = rand() % SCREEN_WIDTH;
+        uint8_t const screenY = rand() % SCREEN_HEIGHT;
+        uint8_t const color = 1 + (rand() % 15);
 
-        drawToScreen(&gameArray[0][0], ARRAY_WIDTH, ARRAY_HEIGHT, 0, 0,
-                     screenX, screenY, rectWidth, rectHeight,
-                     color);
+        drawToScreen(&gameArray[0][0], ARRAY_WIDTH, ARRAY_HEIGHT, 0, 0, screenX,
+                     screenY, rectWidth, rectHeight, color);
     }
 #endif
 
@@ -127,6 +132,10 @@ int main() {
     drawToScreen(&gameArray[0][0], ARRAY_WIDTH, ARRAY_HEIGHT, 10, 10,
                  15, 15, 20, 15,
                  3);
+
+    drawToScreen(&gameArray[0][0], ARRAY_WIDTH, ARRAY_HEIGHT, 10, 10,
+                 25, 2, 20, 15,
+                 4);
 #endif
 
     // Only uppercase letters are correctly shown in PETSCII
