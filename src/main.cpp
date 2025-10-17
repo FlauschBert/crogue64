175be4690ab944ba1aebfc17da19f8e2f453050f
FlauschBert <github@shaula.de>
2025-10-17 17:41:03 +0200

    use uint8_t where possible

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@
 constexpr uint16_t SCREEN_MEMORY = 0x0400;
 constexpr uint16_t COLOR_MEMORY = 0xD800;
 
-constexpr int SCREEN_WIDTH = 40;
-constexpr int SCREEN_HEIGHT = 25;
+constexpr uint8_t SCREEN_WIDTH = 40;
+constexpr uint8_t SCREEN_HEIGHT = 25;
 
-inline int clamp(int value, int min, int max) {
+inline uint8_t clamp(uint8_t value, uint8_t min, uint8_t max) {
     if (value < min) return min;
     if (value > max) return max;
     return value;
@@ -16,17 +16,18 @@ inline int clamp(int value, int min, int max) {
 
 void clearScreen() {
     volatile uint8_t* screenPtr = (volatile uint8_t*)SCREEN_MEMORY;
-    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; ++i) {
+    uint16_t constexpr screenSize = SCREEN_WIDTH * SCREEN_HEIGHT;
+    for (uint16_t i = 0; i < screenSize; ++i) {
         screenPtr[i] = ' ';
     }
 }
 
-void drawText(int posX, int posY, char const* text, uint8_t const size, uint8_t const color) {
+void drawText(uint8_t posX, uint8_t posY, char const* text, uint8_t const size, uint8_t const color) {
     volatile uint8_t* screenPtr = (volatile uint8_t*)SCREEN_MEMORY;
     volatile uint8_t* colorPtr = (volatile uint8_t*)COLOR_MEMORY;
-    for (int i = 0; i < size; ++i) {
-        int const screenX = posX + i - size / 2;
-        int const pos = posY * SCREEN_WIDTH + screenX;
+    for (uint8_t i = 0; i < size; ++i) {
+        uint8_t const screenX = posX + i - size / 2;
+        uint16_t const pos = posY * (uint16_t)SCREEN_WIDTH + screenX;
         if (screenX < SCREEN_WIDTH && posY < SCREEN_HEIGHT) {
             screenPtr[pos] = text[i];
             colorPtr[pos] = color;
@@ -34,42 +35,43 @@ void drawText(int posX, int posY, char const* text, uint8_t const size, uint8_t 
     }
 }
 
-void drawToScreen(const uint8_t* src, int const srcWidth, int const srcHeight,
-                  int const srcOffsetX, int const srcOffsetY, int const screenX,
-                  int const screenY, int const rectWidth, int const rectHeight,
+void drawToScreen(const uint8_t* src, uint8_t const srcWidth, uint8_t const srcHeight,
+                  uint8_t const srcOffsetX, uint8_t const srcOffsetY, uint8_t const screenX,
+                  uint8_t const screenY, uint8_t const rectWidth, uint8_t const rectHeight,
                   uint8_t const rectColor) {
     volatile uint8_t* screenPtr = (volatile uint8_t*)SCREEN_MEMORY;
     volatile uint8_t* colorPtr = (volatile uint8_t*)COLOR_MEMORY;
 
-    int screenXClamp = clamp(screenX, 0, SCREEN_WIDTH - 1);
-    int screenYClamp = clamp(screenY, 0, SCREEN_HEIGHT - 1);
-    int screenXEndClamp = clamp(screenX + rectWidth - 1, 0, SCREEN_WIDTH - 1);
-    int screenYEndClamp = clamp(screenY + rectHeight - 1, 0, SCREEN_HEIGHT - 1);
+    uint8_t screenXClamp = clamp(screenX, 0, SCREEN_WIDTH - 1);
+    uint8_t screenYClamp = clamp(screenY, 0, SCREEN_HEIGHT - 1);
+    uint8_t screenXEndClamp = clamp(screenX + rectWidth - 1, 0, SCREEN_WIDTH - 1);
+    uint8_t screenYEndClamp = clamp(screenY + rectHeight - 1, 0, SCREEN_HEIGHT - 1);
     
     if (screenXClamp > screenXEndClamp || screenYClamp > screenYEndClamp) {
         return;
     }
 
-    int screenWidthClamped = screenXEndClamp - screenXClamp + 1;
-    int screenHeightClamped = screenYEndClamp - screenYClamp + 1;
+    uint8_t screenWidthClamped = screenXEndClamp - screenXClamp + 1;
+    uint8_t screenHeightClamped = screenYEndClamp - screenYClamp + 1;
 
-    int srcOffsetXAdjusted = srcOffsetX + (screenXClamp - screenX);
-    int srcOffsetYAdjusted = srcOffsetY + (screenYClamp - screenY);
+    uint8_t srcOffsetXAdjusted = srcOffsetX + (screenXClamp - screenX);
+    uint8_t srcOffsetYAdjusted = srcOffsetY + (screenYClamp - screenY);
 
-    for (int y = 0; y < screenHeightClamped; ++y) {
-        for (int x = 0; x < screenWidthClamped; ++x) {
-            int screenXPos = x + screenXClamp;
-            int screenYPos = y + screenYClamp;
+    for (uint8_t y = 0; y < screenHeightClamped; ++y) {
+        for (uint8_t x = 0; x < screenWidthClamped; ++x) {
+            
             // we assume that src is large enough to hold the requested data
-            int srcX = x + srcOffsetXAdjusted;
-            int srcY = y + srcOffsetYAdjusted;
-
-            int screenPos = screenYPos * SCREEN_WIDTH + screenXPos;
-            int srcPos = srcY * srcWidth + srcX;
+            uint8_t srcX = x + srcOffsetXAdjusted;
+            uint8_t srcY = y + srcOffsetYAdjusted;
+            uint16_t srcPos = srcY * (uint16_t)srcWidth + srcX;
 
             if (srcPos >= srcWidth * srcHeight || srcPos < 0) {
                 continue;
             }
+
+            uint8_t screenXPos = x + screenXClamp;
+            uint8_t screenYPos = y + screenYClamp;
+            uint16_t screenPos = screenYPos * (uint16_t)SCREEN_WIDTH + screenXPos;
 
             screenPtr[screenPos] = src[srcPos];
             colorPtr[screenPos] = rectColor;
@@ -78,17 +80,17 @@ void drawToScreen(const uint8_t* src, int const srcWidth, int const srcHeight,
 }
 
 void fillCheckBoard(uint8_t* arrayPtr, uint8_t const back, uint8_t const fore,
-                    int const width, int const height) {
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
+                    uint8_t const width, uint8_t const height) {
+    for (uint8_t y = 0; y < height; ++y) {
+        for (uint8_t x = 0; x < width; ++x) {
             arrayPtr[y * width + x] = ((x + y) % 2) ? fore : back;
         }
     }
 }
 
 int main() {
-    constexpr int ARRAY_WIDTH = 60;
-    constexpr int ARRAY_HEIGHT = 50;
+    constexpr uint8_t ARRAY_WIDTH = 60;
+    constexpr uint8_t ARRAY_HEIGHT = 50;
 
     uint8_t gameArray[ARRAY_HEIGHT][ARRAY_WIDTH];
 
@@ -104,11 +106,11 @@ int main() {
     // srand(12345);
 
 #if 1
-    for (int i = 0; i < 100; ++i) {
-        int rectWidth = 5 + (rand() % 16);
-        int rectHeight = 5 + (rand() % 11);
-        int screenX = rand() % (SCREEN_WIDTH - rectWidth);
-        int screenY = rand() % (SCREEN_HEIGHT - rectHeight);
+    for (uint8_t i = 0; i < 100; ++i) {
+        uint8_t rectWidth = 5 + (rand() % 16);
+        uint8_t rectHeight = 5 + (rand() % 11);
+        uint8_t screenX = rand() % (SCREEN_WIDTH - rectWidth);
+        uint8_t screenY = rand() % (SCREEN_HEIGHT - rectHeight);
         uint8_t color = 1 + (rand() % 15);
 
         drawToScreen(&gameArray[0][0], ARRAY_WIDTH, ARRAY_HEIGHT, 0, 0,
